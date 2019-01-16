3565929a2979d2618a040e91fc2a6cf2c93b11b2
Keshav Nangare <keshav.nangare@mindstix.com>
2019-01-16 23:42:43 +0530

    Created the CtlContoller which will enable to execute the ets via REST API.

---
+++ src/main/java/org/opengis/cite/sos20/SOS20.java
@@ -0,0 +1,16 @@
+package org.opengis.cite.sos20;
+
+/**
+ * Contains various constants defined in the SOS 2.0 specification, such as test
+ * run parameters.
+ * 
+ */
+public class SOS20 {
+
+    private SOS20() {
+    }
+
+    /** Supplies reference to SOS capabilities document (URI). */
+    public static final String SERVICE_URL = "service-url";
+
+}
