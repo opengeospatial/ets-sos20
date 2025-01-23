c3ac189423416a879adbc274d1c163122a7a87c4
Benjamin Proß <b.pross@52north.org>
2025-01-23 08:38:10 +0100

    Adjust java code

--- src/main/java/org/opengis/cite/sos20/TestRunArguments.java
+++ src/main/java/org/opengis/cite/sos20/TestRunArguments.java
@@ -20,62 +20,61 @@ import org.w3c.dom.NodeList;
  */
 public class TestRunArguments {
 
-    /**
-     * Validates the given test run arguments. 
-     * 
-     * @param testRunArgs
-     *            A Document containing a set of XML properties; each entry
-     *            (key-value pair) is a test run argument.
-     * @return A valid set of test run arguments.
-     */
-    public static Properties validateArguments(Document testRunArgs) {
-        NodeList entries = testRunArgs.getDocumentElement().getElementsByTagName("entry");
-        if (entries.getLength() == 0) {
-            throw new IllegalArgumentException("No test run arguments.");
-        }
-        
-        Properties args = new Properties();
-        for (int i = 0; i < entries.getLength(); i++) {
-            Element entry = (Element) entries.item(i);
-            args.setProperty(entry.getAttribute("key"), entry.getTextContent().trim());
-        }
-        String capabilitiesUrl = args.getProperty(SOS20.SERVICE_URL);
-        try {
-            URI uriRef = new URI(capabilitiesUrl);
-            if (!uriRef.isAbsolute()) {
-                throw new IllegalArgumentException(String.format("Not an absolute URI: %s", uriRef));
-            }
-        } catch (URISyntaxException e1) {
-            throw new IllegalArgumentException(String.format("Invalid URI reference: %s", capabilitiesUrl));
-        } catch (NullPointerException e2) {
-            throw new IllegalArgumentException(String.format("Missing required argument: %s", SOS20.SERVICE_URL));
-        }
-        args.setProperty(SOS20.WHICH_TESTS, "all");
-        args.setProperty(SOS20.REQUEST_VIA, "GET");
-        return args;
-    }
+	/**
+	 * Validates the given test run arguments.
+	 * @param testRunArgs A Document containing a set of XML properties; each entry
+	 * (key-value pair) is a test run argument.
+	 * @return A valid set of test run arguments.
+	 */
+	public static Properties validateArguments(Document testRunArgs) {
+		NodeList entries = testRunArgs.getDocumentElement().getElementsByTagName("entry");
+		if (entries.getLength() == 0) {
+			throw new IllegalArgumentException("No test run arguments.");
+		}
 
-    /**
-     * Creates a document that contains a simple XML representation of the given
-     * set of properties.
-     * 
-     * @param props
-     *            A set of properties (key-value pairs).
-     * @return A Document node, wherein each property is represented by an
-     *         entry[@key] element.
-     */
-    public static Document propertiesAsDocument(Properties props) {
-        Document doc = null;
-        try {
-            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-            props.storeToXML(outStream, "Test run arguments");
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            doc = builder.parse(new ByteArrayInputStream(outStream.toByteArray()));
-        } catch (Exception e) {
-            Logger.getLogger(TestRunArguments.class.getName()).log(Level.WARNING, "Failed to create Document node. {0}",
-                    e.getMessage());
-        }
-        return doc;
-    }
+		Properties args = new Properties();
+		for (int i = 0; i < entries.getLength(); i++) {
+			Element entry = (Element) entries.item(i);
+			args.setProperty(entry.getAttribute("key"), entry.getTextContent().trim());
+		}
+		String capabilitiesUrl = args.getProperty(SOS20.SERVICE_URL);
+		try {
+			URI uriRef = new URI(capabilitiesUrl);
+			if (!uriRef.isAbsolute()) {
+				throw new IllegalArgumentException(String.format("Not an absolute URI: %s", uriRef));
+			}
+		}
+		catch (URISyntaxException e1) {
+			throw new IllegalArgumentException(String.format("Invalid URI reference: %s", capabilitiesUrl));
+		}
+		catch (NullPointerException e2) {
+			throw new IllegalArgumentException(String.format("Missing required argument: %s", SOS20.SERVICE_URL));
+		}
+		args.setProperty(SOS20.WHICH_TESTS, "all");
+		args.setProperty(SOS20.REQUEST_VIA, "GET");
+		return args;
+	}
+
+	/**
+	 * Creates a document that contains a simple XML representation of the given set of
+	 * properties.
+	 * @param props A set of properties (key-value pairs).
+	 * @return A Document node, wherein each property is represented by an entry[@key]
+	 * element.
+	 */
+	public static Document propertiesAsDocument(Properties props) {
+		Document doc = null;
+		try {
+			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+			props.storeToXML(outStream, "Test run arguments");
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			doc = builder.parse(new ByteArrayInputStream(outStream.toByteArray()));
+		}
+		catch (Exception e) {
+			Logger.getLogger(TestRunArguments.class.getName())
+				.log(Level.WARNING, "Failed to create Document node. {0}", e.getMessage());
+		}
+		return doc;
+	}
 
 }
